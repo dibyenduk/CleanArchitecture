84b30fb167c39b91e20a37941777ba95caea9a16
Dibyendu Kankalia <A4HCHZZ@mmm.com>
2021-11-30 23:50:02 -0600

    Add the missing SAP Project and remove *.SAP from gitignore file

---
+++ src/MES.SAP/Services/Inventory/InventoryRetriever.cs
@@ -0,0 +1,21 @@
+﻿using MES.Core.Services.Inventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MES.SAP.Services.Inventory
+{
+    public class InventoryRetriever
+    {
+        public InventoryDetails[] GetInventory(string materialNbr)
+        {            
+            return new[] { new InventoryDetails() {
+                MaterialNbr = materialNbr,
+                Quantity = new Random(1).Next(1, 50),
+                QuantityUOM = "KG"
+            }};
+        }
+    }
+}
---
+++ src/MES.SAP/Services/Inventory/InventoryService.cs
@@ -0,0 +1,18 @@
+﻿using MES.Core.Services.Inventory;
+using MES.Core.Services.Inventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MES.SAP.Services.Inventory
+{
+    public class InventoryService : IInventoryService
+    {
+        public InventoryDetails[] GetInventory(string materialNbr)
+        {
+            return new InventoryRetriever().GetInventory(materialNbr);
+        }
+    }
+}
